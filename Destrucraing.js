1bafb147078f4290df28586ee1880e975638585c
unknown <nahidujjamanraj@gmail.com>
2024-12-14 12:08:40 +0600

    In this program I know about Destructure

---
+++ Destrucraing.js
@@ -0,0 +1,16 @@
+//  Object Destructure
+ let objet_raj_family={
+    name_father:'Abdus salam',
+    age_father:55,
+    name_mother:"Nurun Nahar begum",
+    age_mother:50,
+    children_total:'Three',
+    First_child:"Barat Ali",
+    Second_child:"Raj",
+    Third_child:"Sumiya"
+
+ }
+ let {name_father:father_name,name_mother,First_child}=objet_raj_family
+ console.log(name_mother,father_name)
+
+//  array  Destructure
