33202ed216d3b35e0c5320556c3386956e64371b
unknown <nahidujjamanraj@gmail.com>
2024-12-17 16:49:30 +0600

    In this program I make program that can conert odd array intoo een array

---
+++ ps3.js
@@ -0,0 +1,9 @@
+// let array is een number 
+let odd_array=[1,3,5,7,9]
+let een_array=[]
+for(let x of odd_array){
+    let final_result=x+1;
+    een_array.push(final_result)
+    
+}
+console.log(een_array)
