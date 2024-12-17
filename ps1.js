1fcb39ad13c39912cf6be89ccd4746285466120f
unknown <nahidujjamanraj@gmail.com>
2024-12-17 16:36:01 +0600

    In this program I practice object

---
+++ ps1.js
@@ -0,0 +1,11 @@
+let data={
+    sophia:{
+        id:33,
+        study:[
+            {school_name:'ABC primary school'},
+            {location:'peters burg'},
+        ]
+    },
+    
+}
+console.log(data.sophia.study[1])
