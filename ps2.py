460a07cff1af3520dc044a9c12362c8cdf164e09
unknown <nahidujjamanraj@gmail.com>
2024-12-21 09:36:10 +0600

    soling square problem

---
+++ ps2.py
@@ -0,0 +1,4 @@
+first_number=int(input('Please gie me first number :'))
+second_number=int(input('Please gie me second number :'))
+sum=first_number+second_number;
+print(sum)
---
+++ ps3.py
@@ -0,0 +1,5 @@
+first_number=float(input('please gie me the first alue of square'))
+second_number=float(input("please gie me the second alue of aquare "))
+square=first_number*second_number;
+ 
+print(square)
