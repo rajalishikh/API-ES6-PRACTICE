db3ed2f3db66f2cd2d55d8cb84b638afd9291cdc
unknown <nahidujjamanraj@gmail.com>
2024-12-21 11:11:35 +0600

    sole een or odd number

--- ps7.py
+++ ps7.py
@@ -1,2 +1,6 @@
 name_user=input('please gibe me your name: ')
 print("Length of user is :",len(name_user))
+
+# count a string 
+bushra= 'she has a 20$ before marriage .after marriage she haa 50$$$$$$'
+print("Total dollar in a str",bushra.count("$"))
---
+++ ps8.py
@@ -0,0 +1,6 @@
+user_input=int(input('please gie me a number :')) 
+if(user_input %2 == 0):
+    print('জোর নামবার ')
+else:
+    print('odd number ')
+    
