a7d15e71ea68f5481a8076ab2ddf5b23300f5058
unknown <nahidujjamanraj@gmail.com>
2024-12-23 13:15:48 +0600

    In this program I know about call back function

---
+++ ps2.js
@@ -0,0 +1,13 @@
+function menu_today(menu,name_parameter){
+    menu(name_parameter)
+
+}
+function type_of_menu(name_cooker){
+    console.log('Alu orta dal',name_cooker)
+}
+function shoshurbari(name){
+    console.log('polao mas baji, goru bhuna, roast,fish curry',name)
+}
+menu_today(type_of_menu,'cooked by My mom')
+menu_today(type_of_menu,'cooked by bushra')
+menu_today(shoshurbari,'cooked by shashuri amma ')
